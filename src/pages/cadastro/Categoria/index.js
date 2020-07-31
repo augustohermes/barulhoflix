7060338ca04121d02a06e4d9fa04b17ca5720cd9
Augusto Hermes <augustohermes@outlook.com>
2020-07-31 06:20:03 -0300

    Atualização

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,15 +29,21 @@ function CadastroCategoria() {
     );
   }
 
+  // ============
+
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
-    fetch(URL);
-      .then(async (respostaDoServidor) => {
-        const resposta = await respostaDoServidor.json();
-        setCategorias([
-          ...resposta,
-        ]);
-    });
+    if (window.location.href.includes('localhost')) {
+      const URL = 'http://localhost:8080/categorias';
+      fetch(URL)
+        .then(async (respostaDoServer) => {
+          if (respostaDoServer.ok) {
+            const resposta = await respostaDoServer.json();
+            setCategorias(resposta);
+            return;
+          }
+          throw new Error('Não foi possível pegar os dados');
+        });
+    }
   }, []);
 
   return (
